cdb15350ecd2c3466e059c9ba9e5a61bbc475f55
TruongGiaTien <truongtien577@gmail.com>
2022-06-15 12:22:09 +0700

    test drawPolygon

--- project 1/main.py
+++ project 1/main.py
@@ -1,6 +1,5 @@
-from tracemalloc import start
 import turtle
-from unicodedata import name
+import numpy as np
 
 def readInputFile(fname):
     with open(fname) as f:
@@ -21,6 +20,11 @@ def readInputFile(fname):
 
             polyList[i] = [int(j) for j in lines]
 
+        # turn 2D array to 3D array
+        polyList = np.array(polyList, dtype="object")
+        for i in range(numOfPoly):
+            polyList[i] = np.array(polyList[i]).reshape(int(len(polyList[i])/2), 2)
+
     return pixel, startGoal, numOfPoly, polyList
 
 # draw grid based on width, height
@@ -30,115 +34,131 @@ def drawGrid(height, width):
 
     height += 1
     width += 1
+    initX = -400
+    initY = -300
 
     turtle.tracer(0)
     # tur.penup()
-    tur.speed(100)
+    tur.speed(6)
     tur.left(90) 
 
     tur.up()
-    tur.setpos(-400, -200)
+    tur.setpos(initX, initY)
     tur.down()
     
     tur.fillcolor("grey")
     tur.begin_fill()
-    tur.forward(height * 30)
+    tur.forward(height * 35)
     tur.right(90)
-    tur.forward(width * 30)
+    tur.forward(width * 35)
     tur.right(90)
-    tur.forward(height * 30)
+    tur.forward(height * 35)
     tur.right(90)
-    tur.forward(width * 30)
-    tur.end_poly()
+    tur.forward(width * 35)
     tur.right(90)
     tur.end_fill()
 
     tur.up()
-    tur.setpos(-370, -170)
+    tur.setpos(initX + 35, initY + 35)
     tur.down()
     tur.fillcolor("white")
     tur.begin_fill()
-    tur.forward(height * 30 - 60)
+    tur.forward(height * 35 - 70)
     tur.right(90)
-    tur.forward(width * 30 - 60)
+    tur.forward(width * 35 - 70)
     tur.right(90)
-    tur.forward(height * 30 - 60)
+    tur.forward(height * 35 - 70)
     tur.right(90)
-    tur.forward(width * 30 - 60)
-    tur.end_poly()
+    tur.forward(width * 35 - 70)
     tur.right(90)
     tur.end_fill()
 
-    x = -400
+    x = initX
     for i in range(width):
         tur.up()
-        tur.setpos(x, -200)
+        tur.setpos(x, initY)
         tur.down()
-        tur.forward(height * 30) 
-        x += 30
-
+        tur.forward(height * 35) 
+        x += 35
 
     tur.right(90)
-    y = -200
+    y = initY
     for i in range(height):
         tur.up()
-        tur.setpos(-400, y)
+        tur.setpos(initX, y)
         tur.down()
 
-        tur.forward(width * 30)
-        y += 30
+        tur.forward(width * 35)
+        y += 35
     
 
-    y = -190
+    y = initY - 10
     for i in range(height):
         tur.up()
-        tur.setpos(-410, y)
-        
-        tur.write(i, align="right",font=('Arial', 10, 'bold'))
-        tur.down()
-
-        y += 30
-
-    x = -390
-    for i in range(width):
-        tur.up()
-        tur.setpos(x, -217)
+        tur.setpos(initX - 10, y)
         
         tur.write(i, align="center",font=('Arial', 10, 'bold'))
         tur.down()
 
-        x += 30
+        y += 35
+
+    x = initX + 10
+    for i in range(width):
+        tur.up()
+        tur.setpos(x, initY - 15)
+        
+        tur.write(i, align="center",font=('Arial', 10, 'bold'))
+        tur.down()
+
+        x += 35
 
     turtle.tracer(1)
     tur.hideturtle()
 
 def fillPixel(x, y, color):
     tur = turtle.Turtle()
+    tur.speed(0)
     tur.hideturtle()
 
     tur.fillcolor("cyan")
 
     tur.up()
-    tur.setpos(-400 + x * 30, -200 + y * 30)
+    tur.setpos(-400 + x * 35, -300 + y * 35)
     tur.down()
 
     tur.fillcolor(color)
     tur.begin_fill()
     for _ in range(4):
-        tur.forward(30)
+        tur.forward(35)
         tur.left(90)
     tur.end_fill()
 
 def drawPolygon(polygonList, height, width):    
-    blockedPosList = [([0] * width)] * height
     turtle.tracer(0)
     for i in range(len(polygonList)):
-        for j in range(0, len(polygonList[i]) - 1, 2):
-            fillPixel(polygonList[i][j], polygonList[i][j + 1], "black")
+        for j in range(len(polygonList[i])):
+            x = polygonList[i][j][0]
+            y = polygonList[i][j][1]
+
+            if j + 1 < len(polygonList[i]):
+                nextX = polygonList[i][j + 1][0]
+                nextY =  polygonList[i][j + 1][1]
+                
+                stepX = stepY = 1
+                if x > nextX:
+                    stepX = -1
+                if y > nextY:
+                    stepY = -1
+                for r in range(x, nextX, stepX):
+                    for s in range(y, nextY, stepY):
+                        fillPixel(r, s, "red")
+    
+    for i in range(len(polygonList)):
+        for j in range(len(polygonList[i])):
+            fillPixel( polygonList[i][j][0], polygonList[i][j][1], "black")        
+
             
     turtle.tracer(1)
-
-    return blockedPosList
 
 def main():
     pixel, startGoal, numOfPoly, polyList = readInputFile(fname="input.txt")
